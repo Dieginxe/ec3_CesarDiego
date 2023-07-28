5be1cb352de32233d72c515923c933f6895a86cc
Dieginxe <134670319+Dieginxe@users.noreply.github.com>
2023-07-28 03:41:23 +0000

    por fin me salio

--- src/main/java/com/example/demo/Instrumento.java
+++ src/main/java/com/example/demo/Instrumento.java
@@ -1,56 +1,56 @@
 package com.example.demo;
 
-import java.util.HashSet;
-import java.util.Set;
-
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OrderBy;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 
 @Entity
-@Table(name="tbl_categoria")
-public class Categoria {
+@Table(name="tbl_instrumento")
+public class Instrumento {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String nombre;
     
-    @OneToMany(targetEntity = Instrumento.class, mappedBy = "categoria")
-    @OrderBy("nombre ASC")
-    private Set<Instrumento> instrumentos = new HashSet<Instrumento>();
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_Categoria")
+    private Categoria categoria;
+
 
     public Integer getId() {
         return id;
     }
 
+
     public void setId(Integer id) {
         this.id = id;
     }
+
 
     public String getNombre() {
         return nombre;
     }
 
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    public Set<Instrumento> getInstrumentos() {
-        return instrumentos;
+
+    public Categoria getCategoria() {
+        return categoria;
     }
 
-    public void setInstrumentos(Set<Instrumento> instrumentos) {
-        this.instrumentos = instrumentos;
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
     }
-
-  
-
-
-    
 }
